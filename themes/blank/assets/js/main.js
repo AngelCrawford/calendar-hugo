eaa9153e79a4ce1986172221e230d8c3cbb35239
AngelCrawford <16148683+AngelCrawford@users.noreply.github.com>
2020-05-28 22:42:01 +0200

    added google holiday calendar for backgrounds

--- themes/blank/assets/js/main.js
+++ themes/blank/assets/js/main.js
@@ -1,14 +1,12 @@
 // $(document).ready(function () {
 
-  console.log("Ready...");
-
-
   document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      plugins: [ 'dayGrid', 'list' ],
+      plugins: [ 'dayGrid', 'list', 'googleCalendar' ],
       timeZone: 'Europe/Berlin',
+      locale: 'de',
       header: {
         left: 'prev,next today',
         center: 'title',
@@ -17,18 +15,39 @@
       defaultDate: '2020-05-12',
       eventLimit: false,
       weekNumbers: true,
+      googleCalendarApiKey: 'AIzaSyDcnW6WejpTOCffshGDDb4neIrXVUA1EAE',
       eventSources: [
         {
+          type: 'party',
           url: '/parties/index.json',
           color: 'yellow',
           textColor: 'black'
         },
         {
+          type: 'concert',
           url: '/concerts/index.json',
           color: 'orange',
           textColor: 'black'
-        }
+        },
+        { 
+          eventDataTransform: function(eventData) {
+            eventData.rendering = 'background';
+          },
+          type: 'holiday',
+          url: 'de.german#holiday@group.v.calendar.google.com' 
+        }        
       ],
+
+      eventRender : function(info) {
+        const props = info.event.extendedProps, rendering = info.event.rendering;
+        
+        if (rendering == "background" && info.el.innerHTML.length == 0) {
+            $(info.el).text(info.event.title);
+          
+            console.log(info.el);
+        }
+      },
+      
       eventTimeFormat: { // like '14:30:00'
         hour: '2-digit',
         minute: '2-digit',
